09103af162793420d3786f0868c27d31345d75c5
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-08-16 17:55:16 +0300

    model User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User (AbstractUser):
+    email = models.EmailField(
+        max_length=150,
+        db_index=True,
+        unique=True,
+        verbose_name='Пользовательский email'
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        db_index=True,
+        verbose_name='Короткое имя',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия пользователя'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = 'username'
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Пользователь',
+        verbose_name_plural = 'Пользователи'
