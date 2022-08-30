e43c2b4b24f8acaac72fed37a5e498b4770fcd1d
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-08-30 15:25:49 +0300

    add data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -2,7 +2,7 @@ import csv
 
 from django.conf import settings
 from django.core.management import BaseCommand
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
@@ -10,6 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data_path = settings.BASE_DIR
+        print('Обработка файла ingredients.csv')
         with open(
             f'{data_path}\\data\\ingredients.csv',
             'r',
@@ -19,3 +20,14 @@ class Command(BaseCommand):
             Ingredient.objects.bulk_create(
                 Ingredient(**data) for data in reader)
         self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+
+        print('Обработка файла tags.csv')
+        with open(
+            f'{data_path}\\data\\tags.csv',
+            'r',
+            encoding='utf-8'
+        ) as file:
+            reader = csv.DictReader(file)
+            Tag.objects.bulk_create(
+                Tag(**data) for data in reader)
+        self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
