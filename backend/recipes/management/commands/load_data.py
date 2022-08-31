62588f30a8bdb0b999efcc16cc48f1e56e13c61f
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-08-31 12:25:54 +0300

    load_data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -19,13 +19,10 @@ class Command(BaseCommand):
                 Ingredient(**data) for data in reader)
         self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
 
-        print('Обработка файла tags.csv')
-        with open(
-            './recipes/data/tags.csv',
-            'r',
-            encoding='utf-8'
-        ) as file:
-            reader = csv.DictReader(file)
-            Tag.objects.bulk_create(
-                Tag(**data) for data in reader)
+        print('Загрузка тэгов')
+        data = [
+            {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
+            {'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
+            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'}]
+        Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
