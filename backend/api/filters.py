1cab27d39fc335d463f33ce39182dd4e514be616
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-09-01 15:13:39 +0300

    filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,8 +1,10 @@
 import django_filters as filters
+from django.core.exceptions import ValidationError
 from django_filters import AllValuesMultipleFilter
 from django_filters import rest_framework as filters
 from django_filters.widgets import BooleanWidget
 from recipes.models import Ingredient, Recipe
+from users.models import User
 
 
 class IngredientFilter(filters.FilterSet):
@@ -14,6 +16,8 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all())
     is_in_shopping_cart = filters.BooleanFilter(
         widget=BooleanWidget(),
         label='В корзине.')
@@ -26,4 +30,23 @@ class RecipeFilter(filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ['is_favorited', 'is_in_shopping_cart']
+        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
+
+
+class TagsMultipleChoiceField(
+        filters.fields.MultipleChoiceField):
+    def validate(self, value):
+        if self.required and not value:
+            raise ValidationError(
+                self.error_messages['required'],
+                code='required')
+        for val in value:
+            if val in self.choices and not self.valid_value(val):
+                raise ValidationError(
+                    self.error_messages['invalid_choice'],
+                    code='invalid_choice',
+                    params={'value': val},)
+
+
+class TagsFilter(filters.AllValuesMultipleFilter):
+    field_class = TagsMultipleChoiceField
