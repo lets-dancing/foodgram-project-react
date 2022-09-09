b988382878f35525444667b4b1c4836657c9e152
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-09-09 15:05:13 +0300

    repair

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -310,7 +310,7 @@
 #             'email', 'id', 'username', 'first_name', 'last_name',
 #             'is_subscribed', 'recipes', 'recipes_count',)
 
-from django.conf import settings
+
 #     def get_recipes(self, obj):
 #         request = self.context.get('request')
 #         limit = request.GET.get('recipes_limit')
@@ -606,3 +606,44 @@ class CheckShoppingCartSerializer(serializers.ModelSerializer):
                 'Этот рецепт отсутствует в корзине'
             )
         return obj
+
+
+class SubscribeRecipeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(
+        source='author.id')
+    email = serializers.EmailField(
+        source='author.email')
+    username = serializers.CharField(
+        source='author.username')
+    first_name = serializers.CharField(
+        source='author.first_name')
+    last_name = serializers.CharField(
+        source='author.last_name')
+    recipes = serializers.SerializerMethodField()
+    is_subscribed = serializers.BooleanField(
+        read_only=True)
+    recipes_count = serializers.IntegerField(
+        read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = (
+            'email', 'id', 'username', 'first_name', 'last_name',
+            'is_subscribed', 'recipes', 'recipes_count',)
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = (
+            obj.author.recipe.all()[:int(limit)] if limit
+            else obj.author.recipe.all())
+        return SubscribeRecipeSerializer(
+            recipes,
+            many=True).data
